3504ffa63f627d15d48f1de2ae520e14bf899da6
Jack Gibbs <jackgibbs66@icloud.com>
2021-08-01 20:38:22 +0100

    Fix insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,21 +1,24 @@
 public class InsertionSort {
     /**
      * This method sorts an unsorted array of integers using the insertion sort algorithm.
+     * In essence, starting with the second element in the array, check whether it is smaller than
+     * the element to the left, if so, continue moving it left while it is smaller than the element
+     * on the left.
      *
      * @param  listToSort  The unsorted list of integers to sort
      * @return n/a - to add soon
      */
     public void sort(int[] listToSort){
-        int pos = listToSort.length - 1;
-        while (pos > 0){
-            int indexNew = pos -1;
-            int valueNew = listToSort[indexNew];
-            while ((indexNew < listToSort.length-1) && (valueNew > listToSort[indexNew+1])){
-                listToSort[indexNew] = listToSort[indexNew+1];
-                indexNew++;
+
+        int element, position;
+        for (int i = 1; i < listToSort.length; i++){
+            element = listToSort[i];
+            position = i - 1;
+            while ((position >= 0) && (element < listToSort[position])){
+                listToSort[position + 1] = listToSort[position];
+                position--;
             }
-            listToSort[indexNew] = valueNew;
-            pos--;
+            listToSort[position + 1] = element;
         }
     }
 }
